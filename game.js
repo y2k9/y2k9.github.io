c41a07da391fe62fd8a6d489fa7cfa8466bc58fe
y2k9 <51354913+y2k9@users.noreply.github.com>
2024-09-08 18:36:58 +0530

    v1.1

--- game.js
+++ game.js
@@ -9,117 +9,192 @@ Game is about catching a goose with a box
 
 // declare variables
 const resetBtn = document.querySelector(".reset");
-const appleBtn = document.querySelector(".create");
+const newBtn = document.querySelector(".create");
 const goose = document.getElementById("hero");
+const glassBox = document.getElementById("glassbox");
 const infoBtn = document.getElementById("infoicon");
 const infoPopUp = document.getElementById("popup");
 const closePopUpBtn = document.getElementById("close");
+const gameOverPopUp = document.getElementById("gameover");
+const restartBtn = document.querySelector(".restartGame");
 
+// const restartButton 
+const directions = [[-1,0],[1,0],[0,-1],[0,1]] // left, right, up, down dx dy respectively
 let score = 0;
-let xGoose = 20;
-let yGoose = 90;
-const stepSize = 5;
-let walkingState = 1;
-let dx = dy = 0;
 
-const apple = {
-    x: 300,
-    y: 300,
-}
+// goose mechanics
+let xGoose = Math.floor((Math.random() * window.innerWidth)); // init position
+let yGoose = Math.floor((Math.random() * window.innerHeight)); 
+goose.style.left = xGoose+"px";
+goose.style.top = yGoose+"px";
+goose.style.display = "block";
+let dx = dy = 0; //init speed 
+const gooseSpeed = 6; 
+let countSteps = 10; //affects turn freq
 
+
+// box mechanics 
+let xBox = 300; // init position
+let yBox = 200; 
+glassBox.style.left = xBox + "px";
+const boxSpeed = 5;
+let dxBox = dyBox = 0;
 
 // initialise game
 let canvas = document.getElementById("game");
-let context = canvas.getContext('2d');
-
+let ctx = canvas.getContext('2d');
 canvas.style.width = (window.innerWidth-20)+"px";
 canvas.style.height = (window.innerHeight-200)+"px";
-
 goose.style.left = "20px";
 goose.style.top = "90px";
-
-
 document.getElementById("score").innerHTML = "Score: " + score;
+
+
+// hiding some elements with no function for now
+newBtn.style.display = "none";
+resetBtn.style.display = "none";
+
+
 
 // buttons
 resetBtn.addEventListener("click", resetGoose);
-// appleBtn.addEventListener("click",createApple);
 infoBtn.addEventListener("click",showInfoPopUp);
+newBtn.addEventListener("click",incrementScore);
 closePopUpBtn.addEventListener("click",hideInfoPopUp);
+restartBtn.addEventListener("click",restartGame);
+
+
+function restartGame() {
+    incrementScore();
+    resetGoose();
+    hideGameOver();
+}
 
 
 
-// move functions
-function stepLeft(){
+
+// move functions for Goose
+function gooseLeft(){
     if (xGoose<20){
         xGoose = window.innerWidth;
     } else {
-        xGoose -= stepSize;
+        xGoose -= gooseSpeed;
         
     }
     goose.style.left = xGoose + "px";
     goose.src = "images/goose.png";
 }
-function stepRight() {
+function gooseRight() {
     if (xGoose > (window.innerWidth-50)) {
         xGoose = 0;
     } else {
-        xGoose += stepSize;
+        xGoose += gooseSpeed;
     }
     goose.style.left = xGoose + "px";
     goose.src = "images/rightWalk1.png";
 }
-function stepDown() {
+function gooseDown() {
     if (yGoose> window.innerHeight-180) {
         yGoose = 80;
     } else {
-        yGoose += stepSize;
+        yGoose += gooseSpeed;
     }
     goose.style.top = yGoose + "px";
     goose.src = "images/downWalk1.png";
 
 }
-function stepUp() {
+function gooseUp() {
     if (yGoose < 90) {
         yGoose = window.innerHeight-180;
     } else {
-        yGoose -= stepSize;
+        yGoose -= gooseSpeed;
     }
     goose.style.top = yGoose + "px";
     goose.src = "images/upWalk1.png";
 }
 
+
+// move functions for Box
+function boxLeft() {
+    if (xBox<20){
+        xBox = window.innerWidth;
+    } else {
+        xBox -= boxSpeed;
+        
+    }
+    glassBox.style.left = xBox + "px";
+}
+function boxRight() {
+    if (xBox > (window.innerWidth-50)) {
+        xBox = 0;
+    } else {
+        xBox += boxSpeed;
+    }
+    glassBox.style.left = xBox + "px";
+}
+function boxDown() {
+    if (yBox> window.innerHeight-180) {
+        yBox = 80;
+    } else {
+        yBox += boxSpeed;
+    }
+    glassBox.style.top = yBox + "px";
+
+}
+function boxUp() {
+    if (yBox < 90) {
+        yBox = window.innerHeight-180;
+    } else {
+        yBox -= boxSpeed;
+    }
+    glassBox.style.top = yBox + "px";
+}
+
+
+
+
+// pop up description
 function showInfoPopUp() {
     infoPopUp.style.display = "block";
 }
-
 function hideInfoPopUp() {
     infoPopUp.style.display = "none";
 }
+function showGameOver() {
+    gameOverPopUp.style.display = "block";
+}
+function hideGameOver() {
+    gameOverPopUp.style.display = "none";
+}
 
 
-// gameplay
+// functionality
 function resetGoose() {
     xGoose = 20;
     yGoose = 90;
-    dx = dy = 0;
-    score = 0;
+    dx = dy = dxBox = dyBox = 0;
     goose.style.left = xGoose + "px";
     goose.style.top = yGoose + "px";
     console.log("Position reset");
 }
 
 
-function createApple() {
-    apple.x = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-    console.log(apple.x);
-    apple.y = Math.floor(Math.random() * (100 - 1 + 1)) + 1;
-    console.log(apple.y);
-
-    context.fillStyle = "red";
-    context.fillRect(apple.x, apple.y, 5, 5);
-    
+function incrementScore() {
+    score++;
+    console.log("The score updated to "+score);
+    document.getElementById("score").innerHTML = "Score: " + score;
 }
+function checkIfCaught() {
+    let sensitivity = 25;
+    if ((Math.abs(xBox-xGoose)<sensitivity)&&(Math.abs(yBox-yGoose)<sensitivity)){
+        showGameOver();
+        dx = dy = dxBox = dyBox = 0;
+    } 
+}
+
+
+
+
 
 // main loop
 function loop() {
@@ -127,59 +202,93 @@ function loop() {
 
     // make the goose walk by velocity
     if (dx==-1){
-        stepLeft();
+        gooseLeft();
     } else if (dx==1){
-        stepRight();
+        gooseRight();
     }
 
     if (dy==-1){
-        stepUp();
+        gooseUp();
     } else if (dy==1){
-        stepDown();
+        gooseDown();
     }
+
+
+    if (dxBox==-1){
+        boxLeft();
+    } else if (dxBox==1){
+        boxRight();
+    }
+
+    if (dyBox==-1){
+        boxUp();
+    } else if (dyBox==1){
+        boxDown();
+    }
+
 
     // check for collision with treat
     // eat treat, increment score
     // create new treat
 
+    randomWalk();
+
+    checkIfCaught();
 
 }
 
-function incrementScore() {
-    score++;
-    console.log("The score is "+score);
-    document.getElementById("score").innerHTML = "Score: " + score;
-}
 
 
 // listen for steps
 document.addEventListener('keydown', function(e) {
     if(e.which === 37) {
         console.log('Left was pressed');
-        dy=0;
-        dx = -1;
+        dyBox=0;
+        dxBox = -1;
         
 
     }
     else if(e.which === 39) {
         console.log('Right was pressed');
-        dx = 1;
-        dy = 0;
+        dxBox = 1;
+        dyBox = 0;
         
     }
     else if(e.which === 38) {
         console.log('Up was pressed');
-        dy = -1;
-        dx = 0;
-        incrementScore();
+        dyBox = -1;
+        dxBox = 0;
     }
     else if(e.which == 40) {
         console.log('Down was pressed');
-        dy = 1;
-        dx = 0;
+        dyBox = 1;
+        dxBox = 0;
     }
 });
 
 
 // execute game
 requestAnimationFrame(loop);
+
+
+//randomWalk
+function randomWalk() { 
+
+    let x = Math.round(Math.random()*4); // 4 means 25% probability it will turn
+    
+    if (countSteps>=20) {
+        if (x==1){
+            let newDirection = directions[Math.floor(Math.random() * directions.length)];
+            dx = newDirection[0];
+            dy = newDirection[1];
+            countSteps = 0;
+        } else {
+            countSteps = 0;
+        }
+        
+    } else {
+        countSteps++;
+    }
+    
+    
+}
